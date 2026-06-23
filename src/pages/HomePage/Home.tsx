cadf9433feb8efe88a56eaf37a8b3547fa65caa2
Denys Ovcharov <denisovcharov88@gmail.com>
2026-06-23 22:48:40 +0200

    updated styles in home page

--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -5,20 +5,40 @@ export default function HomePage() {
     <>
       <section>
         <div className={styles.container}>
-          <div className={styles.left_side}>
-            <h1 className={styles.title}>
-              Unlock your potential with the best{" "}
-              <span className={styles.language}>language</span> tutors
-            </h1>
-            <p>
-              Embark on an Exciting Language Journey with Expert Language
-              Tutors: Elevate your language proficiency to new heights by
-              connecting with highly qualified and experienced tutors.
-            </p>
-            <button className={styles.button}>Get started</button>
+          <div className={styles.title_and_img}>
+            <div className={styles.left_side}>
+              <h1 className={styles.title}>
+                Unlock your potential with the best{" "}
+                <span className={styles.language}>language</span> tutors
+              </h1>
+              <p className={styles.text}>
+                Embark on an Exciting Language Journey with Expert Language
+                Tutors: Elevate your language proficiency to new heights by
+                connecting with highly qualified and experienced tutors.
+              </p>
+              <button className={styles.button}>Get started</button>
+            </div>
+            <div>
+              <img className={styles.image} src="/block.jpg" alt="" />
+            </div>
           </div>
-          <div>
-            <img className={styles.image} src="/block.jpg" alt="" />
+          <div className={styles.box}>
+            <div className={styles.num_descr_box}>
+              <p className={styles.num}>32,000 +</p>
+              <p className={styles.descr}>Experienced tutors</p>
+            </div>
+            <div className={styles.num_descr_box}>
+              <p className={styles.num}>300,000 +</p>
+              <p className={styles.descr}>5-star tutor reviews</p>
+            </div>
+            <div className={styles.num_descr_box}>
+              <p className={styles.num}>120 +</p>
+              <p className={styles.descr}>Subjects taught</p>
+            </div>
+            <div className={styles.num_descr_box}>
+              <p className={styles.num}>200 +</p>
+              <p className={styles.descr}>Tutor nationalities</p>
+            </div>
           </div>
         </div>
       </section>
